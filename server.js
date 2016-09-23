7b4ae9c814d2ca591858330105f1a3b17bee7d34
imdhananjaypandey <pandebetu@gmail.com>
2016-09-23 13:46:00 -0400

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,9 +9,9 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req,res) {
+app.get('/article-one', function (req, res) {
     res.send("article sent and will recieved here.");
-}};
+});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
